8bb5aac4b0d40dd4954d4695bbba3aa11b73a417
森脇 賢子 <shesee@Sapphire.local>
2011-04-08 09:35:35 +0900

    Initial Commit

---
+++ hidspx_posix/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  hidspx_posix
+//
+//  Created by 森脇 賢子 on 11/04/08.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
