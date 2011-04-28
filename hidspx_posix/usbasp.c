2e9922027c9b322e7e59d897d9bed374ab9e6fea
森脇 賢子 <shesee@Sapphire.local>
2011-04-28 13:00:54 +0900

    Enabling USBasp

---
+++ hidspx_posix/usbasp.c
@@ -0,0 +1,9 @@
+//
+//  usbasp.c
+//  hidspx_posix
+//
+//  Created by 森脇 賢子 on 11/04/28.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include "usbasp.h"
---
+++ hidspx_posix/usbasp.h
@@ -0,0 +1,7 @@
+//
+//  usbasp.h
+//  hidspx_posix
+//
+//  Created by 森脇 賢子 on 11/04/28.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
